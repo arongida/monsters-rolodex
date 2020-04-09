84f46dd8cdf4aa7b68777937f80f922133fb3326
ifAronisGida <arkheonos@gmail.com>
2020-04-09 21:31:55 +0200

    Updates

---
+++ precache-manifest.ae1cf21ea4b01fb36e03703f3cd19611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f39db5501a0f37f432a8acaab315d8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "124802b07f6021b4def7",
+    "url": "/monsters-rolodex/static/css/main.67e7c27c.chunk.css"
+  },
+  {
+    "revision": "696cd664e94359881443",
+    "url": "/monsters-rolodex/static/js/2.259342ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.259342ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124802b07f6021b4def7",
+    "url": "/monsters-rolodex/static/js/main.91f57d12.chunk.js"
+  },
+  {
+    "revision": "4ea3fb9132ee769ac0ba",
+    "url": "/monsters-rolodex/static/js/runtime-main.12e1afbb.js"
+  }
+]);
